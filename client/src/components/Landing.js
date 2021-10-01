20c3eb0fa6d6c4d410952015bf2745f7180089c5
maslenm <mattmaslen123@gmail.com>
2021-10-01 14:16:10 +0100

    updated landing page layout and styling based on design

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 const Landing = () => (
   <>
     <header className="landing-header">
-      <h1 className="landing-header__title">EasyFPL.</h1>
+      <h1 className="landing-header__title heading-1">
+        Easy
+        <span className="landing-header__title--purple">FPL</span>
+        <span className="landing-header__title--black">.</span>
+      </h1>
     </header>
 
     <section className="landing-content">
@@ -11,10 +15,11 @@ const Landing = () => (
         A tool made to help you easily and quickly pick the best performing
         players based on the stats you’re interested in.
       </p>
-
       <div className="feature-cards">
         <div className="feature-card">
-          <header className="feature-card__title">Compare</header>
+          <header className="feature-card__title">
+            <h2 className="heading-2">Compare</h2>
+          </header>
           <p className="feature-card__text-content">
             Compare players based on statistics you choose. See how two players
             compare.
@@ -22,7 +27,9 @@ const Landing = () => (
         </div>
 
         <div className="feature-card">
-          <header className="feature-card__title">Pick</header>
+          <header className="feature-card__title">
+            <h2 className="heading-2">Pick</h2>
+          </header>
           <p className="feature-card__text-content">
             Use the picker tool to quickly find the best player for your team in
             each position
@@ -30,7 +37,9 @@ const Landing = () => (
         </div>
 
         <div className="feature-card">
-          <header className="feature-card__title">Save</header>
+          <header className="feature-card__title">
+            <h2 className="heading-2">Save</h2>
+          </header>
           <p className="feature-card__text-content">
             Register to save players to your team to keep track of your picks.
           </p>
@@ -40,8 +49,8 @@ const Landing = () => (
         </div>
       </div>
       <div className="start-cta">
-        <Link to="/Login" role="button" className="btn btn--start">
-          Get Started
+        <Link to="/Home" role="button" className="btn btn--start">
+          Get Started &rarr;
         </Link>
       </div>
     </section>
