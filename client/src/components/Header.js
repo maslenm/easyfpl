5d5dd92bc24c478260f5262c440089030ad5835d
maslenm <mattmaslen123@gmail.com>
2021-10-02 12:04:46 +0100

    updated header title styling colours and margin

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,8 +2,10 @@ import { Link } from "react-router-dom";
 
 const Header = () => (
   <header className="header">
-    <h1 className="header__title">
-      EasyFPL<span>Player Manager Tool</span>
+    <h1 className="header__title heading-1">
+      Easy
+      <span className="header__title--purple">FPL</span>
+      <span className="header__title--span">Player Manager Tool</span>
     </h1>
 
     <Link to="/Login" role="button" className="btn btn--login-register">
