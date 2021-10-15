a87c311497d4c0c657e7e05892a0a85612dd3a8a
maslenm <mattmaslen123@gmail.com>
2021-10-15 21:58:10 +0100

    removed underline from header title

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => (
   <header className="header">
-    <h1 className="header__title heading-1">
+    <h1 className="header__title heading-1--no-line">
       Easy
       <span className="header__title--purple">FPL</span>
       <span className="header__title--span">Player Manager Tool</span>
