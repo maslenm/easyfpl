ef0e4b74bdde2f44ed8bfc1c47ab06846cce71c0
maslenm <mattmaslen123@gmail.com>
2021-10-28 16:09:20 +0100

    fixed alignement of nav-btn in open and closed state with position settings on root. Changed nav <a> tag to <Link>. Added Hooks to MobileNav component and made use of dispatch to toggle menu when nav Link is clicked.

--- client/src/components/MobileNav.js
+++ client/src/components/MobileNav.js
@@ -1,19 +1,31 @@
 import { NavItems } from "./NavItems";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleMenu } from "../actions/MenuAction";
 
-const MobileNav = () => (
-  <>
-    <ul className="mobile-nav-list__items">
-      {NavItems.map((item, index) => {
-        return (
-          <li key={index} className="mobile-nav-list__item">
-            <a className={item.cName} href={item.url}>
-              {item.title}
-            </a>
-          </li>
-        );
-      })}
-    </ul>
-  </>
-);
+const MobileNav = () => {
+  const menu = useSelector((state) => state.menuOpen);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <ul className="mobile-nav-list__items">
+        {NavItems.map((item, index) => {
+          return (
+            <li key={index} className="mobile-nav-list__item">
+              <Link
+                to={item.url}
+                className={item.cName}
+                onClick={() => dispatch(toggleMenu())}
+              >
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
 
 export default MobileNav;
